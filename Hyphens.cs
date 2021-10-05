cb801ed114ef17e1a7b98ac8debc78cbe3a20752
Luoo0101 <48646721+Luoo0101@users.noreply.github.com>
2021-10-05 16:15:30 +0800

    Add files via upload

--- Hyphens.cs
+++ Hyphens.cs
@@ -22,7 +22,6 @@ public class Hyphens : MonoBehaviour
                 //BtnText = text;
 				string Mytxt = ("C:\\Users\\Luo\\Desktop\\sample.txt");
 				string[] str = File.ReadAllLines(Mytxt); 
-				//str = "12345678910";
 				string Nstr = AddHyphens(str);
 				DisplayBtnText.text = Nstr;
             }
@@ -34,18 +33,24 @@ public class Hyphens : MonoBehaviour
 		string buildString = "";
 		int currentIndex;
 		foreach (string i in s){
-		//go through the line every 10 characters
+		//go through the line every 17 characters
 			if (i.Length > 1){
-				for (currentIndex = 0; currentIndex < i.Length; currentIndex += 11)
+				for (currentIndex = 0; currentIndex < i.Length; currentIndex += 17)
 				{
-					if (currentIndex + 11 < i.Length - 1) {
-					//every 10 characters add to our built line and add a "-" and a new line to the end
-						buildString += i.Substring(currentIndex, 11) + "-\n";
-					}
-					else{
+					
+					if (currentIndex + 17 < i.Length - 1) {
+					//every 17 characters add to our built line and add a "-" and a new line to the end
+						if (char.IsWhiteSpace(i[currentIndex + 16])){
+							buildString += i.Substring(currentIndex, 17) + "\n";
+						}
+						else{
+							buildString += i.Substring(currentIndex, 17) + "-\n";
+						}
+					}else{
 					//if we've hit the end, stop the loop
 						break;
 					}
+					
 				}
 				//add the last bit
 				buildString += i.Substring(currentIndex, i.Length - currentIndex)+ "\n";
